8555ba75de5efbc47d55ea8f99f2f7603ad5b2c2
dmheisel <d.m.heisel@gmail.com>
2019-09-23 16:32:16 -0500

    leap year tests set up and functioning

--- leapyear.test.js
+++ leapyear.test.js
@@ -1,15 +1,23 @@
-const leapYear = require('./leapYear')
+const leapYear = require('./leapYear');
 
 describe('testing leapyear function to determine if year is leap year or not', () => {
-  test('year 400 is a leap year', () => {
-    expect(leapYear(400)).toBe(true)
-  })
-  test('year 104 is a leap year', () => {
-    expect(leapYear(104)).toBe(true)
-  })
-  test('year 100 is not a leap year', () => {
-    expect(leapYear(100)).toBe(false)
-  })
-
-
-})
+	test('year 400 is a leap year', () => {
+		expect(leapYear(400)).toBe(true);
+	});
+	test('year 104 is a leap year', () => {
+		expect(leapYear(104)).toBe(true);
+	});
+	test('year 100 is not a leap year', () => {
+		expect(leapYear(100)).toBe(false);
+	});
+	test('year as a string works properly', () => {
+		expect(leapYear('100')).toBe(false);
+		expect(leapYear('800')).toBe(true);
+	});
+	test('no input returns false', () => {
+		expect(leapYear()).toBe(false);
+	});
+	test('string of letters returns false', () => {
+		expect(leapYear('words')).toBe(false);
+	});
+});
